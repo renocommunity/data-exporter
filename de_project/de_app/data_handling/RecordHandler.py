2ff1765660b6c1e0ae5c473b4743364d2b4d3b07
eons44 <eons44@gmail.com>
2020-10-04 16:30:36 -0700

    adds more untested code

---
+++ de_project/de_app/data_handling/RecordHandler.py
@@ -0,0 +1,54 @@
+from ..models.py import Metric, Record
+
+class RecordHandler:
+    def __init__(self, metric_names_to_create = []):
+        self.records = []
+        self.metrics_buffer = [ Metric(name, True) for name in metric_names_to_create ]
+
+    #Create a record with metrics matching those in *this
+    def create_record(self):
+        ret = Record()
+        ret.create_metrics([m.name for m in self.metrics_buffer])
+        ret.validate()
+        return ret
+
+    def get_last_record(self):
+        return records[-1] if records else Record()
+
+    #Add a record to *this
+    def add_record(self, record):
+        if not record.do_metrics_contain(self.metrics_buffer):
+            raise ValueError("Data shape mismatch")
+
+        last_record = self.get_last_record()
+        for m in metrics_buffer:
+            current_metric = record.get_metric(m.name)
+            #skipping validity checks because we can do that.
+            current_metric.total_value = last_record.get_metric(m.name).total_value + current_metric.current_value
+        self.records.append(record)
+
+    def save_records(self):
+        for r in self.records:
+            r.save()
+
+    def clean_data(self):
+        self.records = [ r for r in self.records if r.is_valid ]
+        self.records.sort(key=lambda r: r.timestamp)
+
+    def calculate_trends(self, bin_days=7):
+        self.clean_data() # needs to be done first
+        
+        #calculate moving average of bin_days
+        for r in self.records:
+            first_record_timestamp = r.timestamp - datetime.timedelta(days = bin_days)
+            #TODO: this will be slow on large data sets.
+            record_subset = [ r for r in self.records if r.timestamp > first_record_timestamp and r.timestamp <= r.timestamp ]
+            for m in self.metrics_buffer:
+                m.reset()
+                for s in record_subset:
+                    m.total_value += s.get_metric(m.name).current_value
+                current_metric = r.get_metric(m.name)
+                current_metric.average_value = current_metric.current_value / m.total_value
+
+
+
--- de_project/de_app/models.py
+++ de_project/de_app/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Metric(models.Model):
+    name = models.CharField(default="INVALID_METRIC")
+    current_value = models.IntegerField(default=0)
+    total_value = models.IntegerField(default=0)
+    average_value = models.IntegerField(default=0)
+    is_valid = False
+
+    def __str__(self):
+        return self.name
+
+    def validate(self):
+        #Other validation can be done here
+        self.is_valid = True
+
+    def reset(self):
+        self.current_value = 0
+        self.total_value = 0
+        self.average_value = 0
+
+class Record(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True, blank=True)
+    location = models.CharField(default="")
+    metrics = models.ManyToManyField(Metric)
+    is_valid = False
+
+    def validate(self):
+        #Other validation can be done here
+        self.is_valid = True
+
+    def create_metrics(self, metric_names_to_create = []):
+        for n in metric_names_to_create:
+            toAdd = Metric(name=n)
+            toAdd.validation()
+            metrics.add(toAdd)
+
+    def get_metric(self, metric_name):
+        for m in self.metrics:
+            if m.name == metric_name:
+                return m
+        return Metric()
+
+    def do_metrics_contain(self, metrics):
+        for m in metrics:
+            if not get_metric(m.name).is_valid:
+                return False
+        return True
