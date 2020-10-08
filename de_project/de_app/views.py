b0652720b4db0cae27e085242bcf41f18d3dd879
eons44 <eons44@gmail.com>
2020-10-07 20:30:22 -0700

    Improves url 404 exception catching

--- de_project/de_app/views.py
+++ de_project/de_app/views.py
@@ -11,7 +11,7 @@ def index(request):
 def get_data_as_json_file(request, record_handler_name):
     try:
         record_handler = RecordHandler.objects.get(name=record_handler_name)
-    except:
-        return HttpResponseNotFound()
+    except RecordHandler.DoesNotExist:
+        return HttpResponseNotFound(f"Record handler \"{record_handler_name}\" does not exist")
     json = record_handler.get_records_as_json()
     return HttpResponse(json, content_type='application/json')
